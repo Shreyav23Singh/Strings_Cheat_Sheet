c538bcb46f58ec16139961883897fc1363db15e3
Shreya Singh <66567410+Shreyav23Singh@users.noreply.github.com>
2023-03-22 09:17:02 +0530

    Create charAtExamole.java

---
+++ charAtExamole.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    String str ="success";
+    char ch=str.charAt(3);
+    char ch1=str.charAt(6);
+    char ch2=str.charAt(0);
+    System.out.println(ch);
+     System.out.println(ch1);
+     System.out.println(ch2);
+  }
+}
